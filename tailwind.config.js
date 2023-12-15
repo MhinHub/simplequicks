376a17bc3901e93ec57600530f66c5c17d008ba0
MhinHub <iskandarm20d@student.unhas.ac.id>
2023-12-16 00:04:32 +0800

    add: plugin & colors tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui'
+import tailwindAnimate from 'tailwindcss-animate'
+import flowbite from 'flowbite/plugin'
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
   ],
-  plugins: [daisyui],
+  theme: {
+    extend: {
+      colors: {
+        'primary-blue': '#2F80ED',
+        'primary-dark': '#4F4F4F',
+        'primary-neutral': '#828282',
+        'primary-light': '#E0E0E0',
+        'indicator-orange': '#F8B76B',
+        'indicator-red': '#EB5757',
+        'indicator-yellow': '#F2C94C',
+        'indicator-blue': '#8785FF',
+        'sticker-blue': '#E9F3FF',
+        'sticker-orange': '#FDCFA4',
+        'sticker-barely': '#F9E9C3',
+        'sticker-waterwing': '#AFEBDB',
+        'sticker-celery': '#CBF1C2',
+        'sticker-bright': '#FDE3A7',
+        'sticker-lavender': '#CFCEF9',
+        'sticker-sugarpill': '#F9E0FD',
+      }
+    }
+  },
+  plugins: [daisyui, tailwindAnimate, flowbite],
 }
 
