9d8101fc6da50b4f7b969a4e80eafbbdae26c141
MhinHub <iskandarm20d@student.unhas.ac.id>
2023-12-16 00:05:51 +0800

    add: Speed Dial component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import SpeedDial from "./components/SpeedDial";
+
 function App() {
   return (
     <main>
       <section className="h-screen w-screen bg-[#0f8a69] grid place-content-center">
-        <h1 className="text-white font-bold text-7xl">Hello World!</h1>
+        <SpeedDial
+          icons={[
+            {
+              name: "Task",
+              icon: "mdi:book-open-outline",
+              bgColor: "bg-red-400",
+              color: "indicator-orange",
+            },
+            {
+              name: "Inbox",
+              icon: "ic:outline-question-answer",
+              bgColor: "bg-purple-500",
+              color: "indicator-blue",
+            },
+          ]}
+        />
+        <h1 className="text-white font-bold text-7xl">Simple Quicks</h1>
       </section>
     </main>
   );
