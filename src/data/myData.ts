4c34c7384109dc246ca89f403bd453934c822748
MhinHub <iskandarm20d@student.unhas.ac.id>
2023-12-18 02:14:29 +0800

    remove: all useless api ref

--- src/data/myData.ts
+++ src/data/myData.ts
@@ -1,9 +1,12 @@
-import client from "../utils/api/chat";
+import { StreamChat } from "stream-chat";
+
+export const apiKey = import.meta.env.VITE_STREAM_API_KEY;
+
+const client = StreamChat.getInstance(apiKey);
 
 export const userId = "ali";
 export const userName = "Ali";
 export const userToken = client.devToken(userId);
-export const apiKey = import.meta.env.VITE_STREAM_API_KEY;
 export const image = `https://getstream.io/random_png/?id=${userId}&name=${userName}`;
 
 // JSON
--- src/utils/api/chat.ts
+++
@@ -1,8 +0,0 @@
-import { StreamChat } from "stream-chat";
-
-const apiKey = import.meta.env.VITE_STREAM_API_KEY;
-// const secretKey = import.meta.env.VITE_STREAM_SECRET_KEY;
-
-const client = StreamChat.getInstance(apiKey);
-
-export default client;
